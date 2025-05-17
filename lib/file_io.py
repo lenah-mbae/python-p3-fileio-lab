af1bf7fa598d763b055e3cb18aa99d74267232ed
Lenah Mbae <lenahmbae22@gmail.com>
2025-05-17 14:58:41 +0300

    Io

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    file_path = f"{file_name}.txt"
+    with open(file_path, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_path = f"{file_name}.txt"
+    with open(file_path, mode='a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = f"{file_name}.txt"
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        return file.read()
